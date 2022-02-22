90c22202b791730fd61e44d4f633c0c1a007c94c
Lawrence C <lawrencecaudle870@gmail.com>
2022-02-22 10:26:06 -0500

    updated async function in javascript

--- js/index.js
+++ js/index.js
@@ -4,21 +4,21 @@ const mainAdviceId = document.querySelector("#main-advice-id");
 const mainAdvice = document.querySelector("#main-advice");
 const mainBtn = document.querySelector("#main-btn");
 
-mainBtn.addEventListener("click", getAdvice);
-
 window.addEventListener("load", getAdvice)
 
-function getAdvice() {
-    fetch("https://api.adviceslip.com/advice")
-        .then((response) => {
-            return response.json();
-        })
-        .then((adviceData) => {
-            const adviceObj = adviceData.slip;
-            mainAdviceId.innerHTML = `<p>ADVICE #${adviceObj.id}</p>`;
-            mainAdvice.innerHTML = `<p>"${adviceObj.advice}"</p>`;
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+mainBtn.addEventListener("click", getAdvice);
+
+async function getAdvice() {
+    try {
+    const request = await fetch("https://api.adviceslip.com/advice");
+    const response = await request.json();
+
+    let adviceId = response.slip.id;
+    let adviceBody = response.slip.advice;
+
+    mainAdviceId.innerHTML = `<p>ADVICE #${adviceId}</p>`;
+    mainAdvice.innerHTML = `<p>"${adviceBody}"</p>`;
+    } catch(error) {
+        console.log(`You've received an ${error}`);
+    } 
 }
